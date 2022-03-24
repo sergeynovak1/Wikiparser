43f1f0491fe2af469e6ea58d3b7f9bfe0f09a5ed
Sergey <ssnovak@stud.kpfu.ru>
2022-03-24 22:31:00 +0300

    pylint 4

--- src/tree_hash_map.py
+++ src/tree_hash_map.py
@@ -1,4 +1,5 @@
 CONST_TO_INCREASE = 0.8
+'''переменная для увеличения размера массива'''
 
 class LinkedList:
     """односвязный список"""
@@ -59,7 +60,9 @@ class LinkedList:
 
 
 class HashMap:
+    """Hash-Map"""
     def __init__(self, _size=100):
+        """инициализация"""
         self._inner_list = []
         for _ in range(_size):
             self._inner_list.append(LinkedList())
@@ -67,6 +70,7 @@ class HashMap:
         self._cnt = 0
 
     def __getitem__(self, key):
+        """получение"""
         linked_list = self._inner_list[hash(key) % self._size]
         node = linked_list.head
         while node:
@@ -75,6 +79,7 @@ class HashMap:
             node = node.next_node
 
     def __setitem__(self, key, value):
+        """добавление"""
         self._inner_list[hash(key) % self._size].append(key, value)
         self._cnt += 1
         # увеличиваем массив
@@ -91,10 +96,12 @@ class HashMap:
             self._inner_list = new_inner_list
 
     def __delitem__(self, key):
+        """удаление"""
         self._inner_list[hash(key) % self._size].delkey(key)
         self._cnt -= 1
 
     def __str__(self):
+        """вывод"""
         mas = []
         for index in range(len(self._inner_list)):
             node = self._inner_list[index].head
@@ -111,91 +118,91 @@ class HashMap:
         return '\n'.join(map(str, mas))
 
     def get_cnt(self):
+        """размер"""
         return self._cnt
     def get_list(self):
+        """лист"""
         return self._inner_list
 
 
 class TreeMap:
-	root = None
-	class NodeTree:
-		def __init__(self, key, element, left = None, right = None):
-			self.element = element
-			self.left = left
-			self.right = right
-			self.key = key
+    root = None
+    class NodeTree:
+        def __init__(self, key, element, left = None, right = None):
+            self.element = element
+            self.left = left
+            self.right = right
+            self.key = key
 
-	def __init__(self):
-		self.list = []
-		self.root = None
+    def __init__(self):
+        self.list = []
+        self.root = None
 
-	def __getitem__(self, key):
-		def getitem(node):
-			if node is None:
-				raise KeyError
-			if key == node.key:
-				return node.element
-			elif key < node.key:
-				return getitem(node.left)
-			else:
-				return getitem(node.right)
-		return getitem(self.root)
+    def __getitem__(self, key):
+        def getitem(node):
+            if node is None:
+                raise KeyError
+            if key == node.key:
+                return node.element
+            if key < node.key:
+                return getitem(node.left)
+            return getitem(node.right)
+        return getitem(self.root)
 
-	def __setitem__(self, key, element):
-		def setitem(node):
-			if node is None:
-				self.list.append(key)
-				return self.NodeTree(key, element)
-			else:
-				if key == node.key:
-					node.element = element
-				elif key < node.key:
-					node.left = setitem(node.left)
-				else:
-					node.right = setitem(node.right)
-				return node
-		self.root = setitem(self.root)
+    def __setitem__(self, key, element):
+        def setitem(node):
+            if node is None:
+                self.list.append(key)
+                return self.NodeTree(key, element)
+            if key == node.key:
+                node.element = element
+            elif key < node.key:
+                node.left = setitem(node.left)
+            else:
+                node.right = setitem(node.right)
+            return node
+        self.root = setitem(self.root)
 
-	@staticmethod
-	def find_min_node(node):
-		if node.left is not None:
-			return TreeMap.find_min_node(node.left)
-		return node
+    @staticmethod
+    def find_min_node(node):
+        if node.left is not None:
+            return TreeMap.find_min_node(node.left)
+        return node
 
-	def __delitem__(self, key):
-		def delitem(node, key):
-			if node is None:
-				raise KeyError
-			if key < node.key:
-				node.left = delitem(node.left, key)
-				return node
-			elif key > node.key:
-				result = delitem(node.right, key)
-				node.right = result
-				return node
-			else:
-				if node.left is None and node.right is None:
-					return None
-				elif node.left is not None and node.right is None:
-					return node.left
-				elif node.left is None and node.right is not None:
-					return node.right
-				else:
-					min_node = TreeMap.find_min_node(node.right)
-					node.key = min_node.key
-					node.value = min_node.value
-					node.right = delitem(node.right, min_node.key)
-					return node
-		self.root = delitem(self.root, key)
+    def __delitem__(self, key):
+        def delitem(node, key):
+            if node is None:
+                raise KeyError
+            if key < node.key:
+                node.left = delitem(node.left, key)
+                return node
+            elif key > node.key:
+                result = delitem(node.right, key)
+                node.right = result
+                return node
+            else:
+                if node.left is None and node.right is None:
+                    return None
+                elif node.left is not None and node.right is None:
+                    return node.left
+                elif node.left is None and node.right is not None:
+                    return node.right
+                else:
+                    min_node = TreeMap.find_min_node(node.right)
+                    node.key = min_node.key
+                    node.value = min_node.value
+                    node.right = delitem(node.right, min_node.key)
+                    return node
+        self.root = delitem(self.root, key)
 
-	def __str__(self):
-		def return_key(node):
-			return f" {node.key}:{node.element} "
-		def return_all(node):
-			if node is None:
-				return ''
-			return return_all(node.left) + return_key(node) + return_all(node.right)
-		return return_all(self.head)
+    def __str__(self):
+        def return_key(node):
+            return f" {node.key}:{node.element} "
+        def return_all(node):
+            if node is None:
+                return ''
+            return return_all(node.left) + return_key(node) + return_all(node.right)
+        return return_all(self.head)
 
-	def get_list(self):
-		return self.list
+    def get_list(self):
+        return self.list
